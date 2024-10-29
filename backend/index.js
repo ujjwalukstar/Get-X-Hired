89e71f8db29815f06d135eec59ec104215be854d
Ujjwal Ukstar <101917520+ujjwalukstar@users.noreply.github.com>
2024-10-29 19:25:33 +0530

    Update index.js
    new backend url updated

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://get-x-hired.onrender.com',
+    origin:'https://get-x-hired-backend.onrender.com',
     credentials:true
 }
 
